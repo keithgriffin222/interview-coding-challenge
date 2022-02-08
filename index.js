8d48d2415e0389ba9ac616fa5be255524c12aa57
keithgriffin222 <siouxfallxenos@gmail.com>
2022-02-09 01:01:16 +1100

    added changes

--- index.js
+++ index.js
@@ -33,11 +33,11 @@ for (let i = 0; i < carts.length; i++) {
 }
 
 
-//NOTE: This function shown between 17:00-18:50 (Part 2/5) is irrelvant and not used.
+//NOTE: This function shown between 17:00-18:50 (Part 2/5).
 //This function checks how many items are added to the local storage (in case the page is refreshed while shopping).
-// function onLoadCartNumbers() {
-//     let productNumbers = localStorage.getItem('cartNumbers');
-// }
+function onLoadCartNumbers() {
+     let productNumbers = localStorage.getItem('cartNumbers');
+ }
 
 // This function tracks how many items are being added to the cart.
 function cartNumbers(products) {
@@ -97,3 +97,49 @@ function totalCost(products) {
         localStorage.setItem("totalCost", products.price);
     }
 }
+
+//This function will display the quantity of cart items in local storage and on the webpage.
+function displayCart() {
+    let cartItems = localStorage.getItem("productsInCart");
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector
+    (".container-products");
+    let cartCost = localStorage.getItem('totalCost');
+
+    console.log(cartItems);
+    if ( cartItems && productContainer ) {
+        productContainer.innerHTML = '';
+        Object.values(cartItems).map(item => {
+            productContainer.innerHTML += `
+            <div class = "product">
+                <ion-icon name="trash-outline"></ion-icon>
+                <img src ="./images/${item.tag}.jpg">
+                <span>${item.name}</span>
+            </div>
+            <div class = "price">$${item.price}.00</div>
+            <div class = "quantity">
+                <ion-icon name="caret-back-outline"></ion-icon>
+                <span>${item.inCart}<span>
+                <ion-icon name="caret-forward-outline"></ion-icon>
+            </div>
+            <div class = "total">
+                $${item.inCart * item.price}.00
+            </div>
+            `;
+        });
+
+        productContainer.innerHTML += `
+            <div class = "basketTotalContainer">
+                <h4 class = "basketTotalTitle">
+                    Basket Total
+                </h4>
+                <h4 class = "basketTotal">
+                    $${cartCost}.00
+                </h4>
+        `;
+    }
+}
+
+//Call displayCart function whenever the page loads.
+onLoadCartNumbers();
+displayCart();
